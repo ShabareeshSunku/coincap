43fd5cf12dc03ccec4193fe529e55953181c0f3f
sowmya144 <50220924+sowmya144@users.noreply.github.com>
2019-06-25 11:33:32 -0400

    Added Exchanges

--- src/Components/Exchanges.js
+++ src/Components/Exchanges.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchExchanges } from "../actions";
 
-export default function Exchanges(){
-    return <h1>I am Exchanges page</h1>
+class Exchanges extends Component {
+  componentDidMount() {
+    this.props.fetchExchanges();
+  }
+  render() {
+    const exchanges = this.props.exchanges;
+    const { items = [], loading = false } = exchanges;
+    const headings = [
+      "Rank",
+      "Name",
+      "Trading Pairs",
+      "Top Pair",
+      "Volume(24Hr)",
+      "Total%",
+      "Status"
+    ];
+    return (
+      <div className="list-container">
+        {loading ? (
+          <div className="loader" />
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                {headings.map((heading, index) => (
+                  <th
+                    className={heading === "Rank" ? "align-center" : ""}
+                    key={"" + index}
+                  >
+                    {heading}
+                  </th>
+                ))}
+              </tr>
+              {items.map((item, index) => {
+                return (
+                  <tr key={"" + index}>
+                    <td className="align-center">{item.rank}</td>
+                    <td>{item.name}</td>
+                    <td>{item.tradingPairs}</td>
+                    <td>{item.topPair}</td>
+                    <td>${item.volume}</td>
+                    <td>{item.totalPercent}</td>
+                    <td>{item.status}</td>
+                  </tr>
+                );
+              })}
+            </thead>
+          </table>
+        )}
+      </div>
+    );
+  }
 }
+const mapStateToProps = (state = {}) => {
+  return {
+    exchanges: state.exchanges || {}
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchExchanges: () => dispatch(fetchExchanges())
+  };
+};
+const ConnectedExchanges = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Exchanges);
+
+export default ConnectedExchanges;
--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,21 @@
 function abbrNumber(actual, fixed) {
-    const num = parseFloat(actual)
-    if (num === null) { return null; } // terminate early
-    if (num === 0) { return '0'; } // terminate early
-    fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
-    var b = (num).toPrecision(2).split("e"), // get power
-        k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
-        c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
-        d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
-        e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
-    return e;
+  const num = parseFloat(actual);
+  if (num === null) {
+    return null;
+  } // terminate early
+  if (num === 0) {
+    return "0";
+  } // terminate early
+  fixed = !fixed || fixed < 0 ? 0 : fixed; // number of decimal places to show
+  var b = num.toPrecision(2).split("e"), // get power
+    k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+    c =
+      k < 1
+        ? num.toFixed(0 + fixed)
+        : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+    d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+    e = d + ["", "K", "M", "B", "T"][k]; // append power
+  return e;
 }
 /**
  * Function to process Assets
@@ -16,28 +23,43 @@ function abbrNumber(actual, fixed) {
  * @returns {Array} properly formatted asset data pushed into an array
  */
 function processAssets(assets = []) {
-    //variable to hold the formatted(processed) data
-    let formattedAssets = []
-    let assetLen = assets.length
-    for (let i = 0; i < assetLen; i++) {
-        let ithAsset = assets[i]
-        formattedAssets.push({
-            name: ithAsset.name,
-            rank: ithAsset.rank,
-            symbol: ithAsset.symbol,
-            supply: abbrNumber(ithAsset.supply, 2),
-            marketCap: abbrNumber(ithAsset.marketCapUsd, 2),
-            volume: abbrNumber(ithAsset.volumeUsd24Hr, 2),
-            price: parseFloat(ithAsset.priceUsd).toFixed(2),
-            vwap24Hr: abbrNumber(ithAsset.vwap24Hr, 2),
-            change: parseFloat(ithAsset.changePercent24Hr).toFixed(2)
-        })
-    }
-    return {
-        assets: formattedAssets
-    }
+  //variable to hold the formatted(processed) data
+  let formattedAssets = [];
+  let assetLen = assets.length;
+  for (let i = 0; i < assetLen; i++) {
+    let ithAsset = assets[i];
+    formattedAssets.push({
+      name: ithAsset.name,
+      rank: ithAsset.rank,
+      symbol: ithAsset.symbol,
+      supply: abbrNumber(ithAsset.supply, 2),
+      marketCap: abbrNumber(ithAsset.marketCapUsd, 2),
+      volume: abbrNumber(ithAsset.volumeUsd24Hr, 2),
+      price: parseFloat(ithAsset.priceUsd).toFixed(2),
+      vwap24Hr: abbrNumber(ithAsset.vwap24Hr, 2),
+      change: parseFloat(ithAsset.changePercent24Hr).toFixed(2)
+    });
+  }
+  return {
+    assets: formattedAssets
+  };
 }
 
-export {
-    processAssets
+function processExchanges(exchanges = []) {
+  let formattedExchanges = [];
+  let exchangeLen = exchanges.length;
+  for (let i = 0; i < exchangeLen; i++) {
+    let ithExchange = exchanges[i];
+    formattedExchanges.push({
+      name: ithExchange.name,
+      rank: ithExchange.rank,
+      tradingPairs: ithExchange.tradingPairs,
+      volume: abbrNumber(ithExchange.volumeUsd, 2),
+      totalPercent: parseFloat(ithExchange.percentTotalVolume).toFixed(2)
+    });
+  }
+  return {
+    exchanges: formattedExchanges
+  };
 }
+export { processAssets, processExchanges };
